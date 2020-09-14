5148b09bc374f8c5e3462aedbc61624d158e059f
leondante-ctrl <maxgamerdu@gmail.com>
2020-09-14 16:17:52 +0300

    wrote in quotes to show on home page

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
+  quotes = [
+    new Quote(1, 'Excuses Will Turn Your Dreams into Dust', 'Joseph Chiuri', 'Abdi ', 0, 0, new Date(2019, 0, 11)),
+    new Quote(2, 'Think about every problem, every challenge, we face. The solution to each starts with education', 'George bush', 'Annie', 0, 0, new Date(2019, 0, 9)),
+    new Quote(3, 'What does not kill you makes you stronger', 'Nelson Mandela', 'Brian', 0, 0, new Date(2019, 0, 11)),
+  ];
+
+
+  addNewQuote(quote) {
+    this.quotes.push(quote);
+  }
+  deleteQuote(isComplete, index) {
+    if (isComplete) {
+      const toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}`);
+      if (toDelete) {
+        this.quotes.splice(index, 1);
+      }
+    }
+  }
+  //My methods defined after the properties
+  toogleDetails(index) {
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+  upvote(index) {
+    this.quotes[index].upvotes++;
+  }
+  downvote(index) {
+    this.quotes[index].downvotes++;
+  }
   constructor() { }
 
   ngOnInit() {
